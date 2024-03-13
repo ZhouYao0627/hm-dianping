b578632d9b4ea79742393d0945599c5f13543b4a
ZhouYao0627 <1607435943@qq.com>
2024-03-13 15:18:20 +0800

    Initial commit

---
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -0,0 +1,102 @@
+package com.hmdp.controller;
+
+
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.UserInfo;
+import com.hmdp.service.IUserInfoService;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private IUserService userService;
+
+    @Resource
+    private IUserInfoService userInfoService;
+
+    /**
+     * 发送手机验证码
+     */
+    @PostMapping("code")
+    public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
+        // TODO 发送短信验证码并保存验证码
+        return Result.fail("功能未完成");
+    }
+
+    /**
+     * 登录功能
+     * @param loginForm 登录参数，包含手机号、验证码；或者手机号、密码
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
+        // TODO 实现登录功能
+        // 1.获取手机号等信息
+        String phone = loginForm.getPhone();
+        // 1.1判断手机号是否符合要求
+        boolean phoneInvalid = RegexUtils.isPhoneInvalid(phone);
+        // 不符合 -> 返回错误信息
+        if (!phoneInvalid) {
+            return Result.fail("手机号格式有误，请重新输入！");
+        }
+        // 符合
+
+        // 2.发送验证码
+
+        // 3.判断手机号和验证码都是否符合要求
+        // 3.1不符合 -> 返回错误信息
+        // 3.2符合
+
+        //4.将信息存入数据库
+
+
+        return Result.fail("功能未完成");
+    }
+
+    /**
+     * 登出功能
+     * @return 无
+     */
+    @PostMapping("/logout")
+    public Result logout(){
+        // TODO 实现登出功能
+        return Result.fail("功能未完成");
+    }
+
+    @GetMapping("/me")
+    public Result me(){
+        // TODO 获取当前登录的用户并返回
+        return Result.fail("功能未完成");
+    }
+
+    @GetMapping("/info/{id}")
+    public Result info(@PathVariable("id") Long userId){
+        // 查询详情
+        UserInfo info = userInfoService.getById(userId);
+        if (info == null) {
+            // 没有详情，应该是第一次查看详情
+            return Result.ok();
+        }
+        info.setCreateTime(null);
+        info.setUpdateTime(null);
+        // 返回
+        return Result.ok(info);
+    }
+}
